6d8a8847b6960fb2d3fbfec6f4b2774d17ed57c8
enjin <kraytenjin@gmail.com>
2021-05-21 23:39:37 +0900

    CORS対応実施

--- src/api_server.js
+++ src/api_server.js
@@ -3,6 +3,13 @@ const apiServer = express();
 const port = 3333;
 
 apiServer.get('/users', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+  
   // JSON形式で返却する
   res.json({
     username: 'enjin'
